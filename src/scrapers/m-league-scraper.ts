4ba9224ebb663d8977b8f6601c4c111715ce2b3d
suzuryo <suzuryo@gmail.com>
2025-10-06 07:25:56 +0900

    refactor: 未使用のfetchPeriodsメソッドを削除
    - fetchPeriodsメソッドはfetchAllと重複しており使用されていないため削除
    - Period型のimportも不要になったため削除

--- src/scrapers/m-league-scraper.ts
+++ src/scrapers/m-league-scraper.ts
@@ -1,6 +1,6 @@
 import { M_LEAGUE_CONFIG } from '../config'
 import { hasScheduleData, parseSchedules } from '../parsers/html-parser'
-import type { Period, Schedule } from '../types/schedule'
+import type { Schedule } from '../types/schedule'
 
 export class MLeagueScraper {
   /**
@@ -43,25 +43,6 @@ export class MLeagueScraper {
     const allSchedules: Schedule[] = []
 
     for (const period of M_LEAGUE_CONFIG.periods) {
-      const monthSchedules = await this.fetchMonth(period.year, period.month)
-      allSchedules.push(...monthSchedules)
-      console.log(
-        `  Found ${monthSchedules.length} matches for ${period.year}/${period.month}`,
-      )
-    }
-
-    return allSchedules
-  }
-
-  /**
-   * Fetch schedules for custom periods
-   * @param periods - Array of Period objects to fetch
-   * @returns Array of Schedule objects
-   */
-  async fetchPeriods(periods: Period[]): Promise<Schedule[]> {
-    const allSchedules: Schedule[] = []
-
-    for (const period of periods) {
       const monthSchedules = await this.fetchMonth(period.year, period.month)
       allSchedules.push(...monthSchedules)
       console.log(
